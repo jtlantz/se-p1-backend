9e4ffe59f5f08692605161b387e132a912268758
justyn lantz <justyn@DESKTOP-7JDRPAI.localdomain>
2023-01-13 22:58:36 +0700

    add: models for vending machine, edit: some pages while still learning django

--- app/backend/portal/models.py
+++ app/backend/portal/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class VendingMachine(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    building = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    location = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Stock(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    vending_machine = models.ForeignKey(VendingMachine, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.name
--- app/backend/portal/views.py
+++ app/backend/portal/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("main page for portal")
+
+def second_page(request):
+    return HttpResponse("If you can read this you are on the second page")

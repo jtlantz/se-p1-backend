f8d670edc2ffa3a3102ef68acf381a0616086964
justyn lantz <justyn@DESKTOP-7JDRPAI.localdomain>
2023-01-13 23:06:15 +0700

    add: some comments for the models, move Dockerfile inside django directory so docker-compose can be used later w/ a database

--- app/backend/portal/models.py
+++ app/backend/portal/models.py
@@ -1,27 +1,37 @@
 from django.db import models
 
-# Create your models here.
-
+"""
+Each vending machine has a unique ID to identify the vending machine, this is auto generated
+More information associated with the vending machine but the required info is listed below
+More information may be added later on
+"""
 class VendingMachine(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     building = models.CharField(max_length=50)
     floor = models.IntegerField()
     location = models.CharField(max_length=50)
-    def __str__(self):
-        return self.name
 
+"""
+Each product, e.g. Snickers, Aquarius, etc... is defined here.
+This is indented to indicate the product that we have on hand in total
+The stock here DOES NOT mean we have this stock in a certain vending machine
+This is just a list of on-hand stock
+"""
 class Product(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=50)
-    price = models.IntegerField()
-    quantity = models.IntegerField()
-    def __str__(self):
-        return self.name
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    stock = models.IntegerField()
 
+"""
+The stock is a association between the vending machine and the product
+The current stock on hand inside the vending machine is in this table
+"""
 class Stock(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
+    #The vending machine that this stock is associated with
     vending_machine = models.ForeignKey(VendingMachine, on_delete=models.CASCADE)
+    #the name of the product can be inherited from the product table
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    #How much stock is currently in the vending machine
     quantity = models.IntegerField()
-    def __str__(self):
-        return self.name

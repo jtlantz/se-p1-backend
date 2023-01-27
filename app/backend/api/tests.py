f5075ee57177e8bad94a9889a95b6bccf2d921c8
justyn lantz <justyn@DESKTOP-7JDRPAI.localdomain>
2023-01-27 09:34:09 +0700

    add some things for testing functions, currently not working

--- app/backend/api/tests.py
+++ app/backend/api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .views import *
+from .models import *
 
 # Create your tests here.
+
+TEST_BUILDING = "test building"
+TEST_FLOOR = 1
+TEST_LOCATION = "test room"
+
+
+class TestCreateVendingMachine(TestCase):
+
+    def setUp(self):
+        VendingMachine(building=TEST_BUILDING, floor=TEST_FLOOR, location=TEST_LOCATION)
+
+    def test_vending_machine_created(self):
+        vm = VendingMachine.objects.filter(building="Test Building")
+        self.assertTrue(vm.building == TEST_BUILDING)
+        self.assertTrue(vm.floor == TEST_FLOOR)
+        self.assertTrue(vm.location == TEST_LOCATION)
+        vm.delete()

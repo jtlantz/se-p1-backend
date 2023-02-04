e454902c652cdca185ec183de81c78b83a19ca41
jtlantz <jtlantz@github.com>
2023-02-04 19:15:03 +0700

    fix: some security issues from sonarcloud, disable debug mode, change post methods to get methods when redirecting them to get only pages

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=^y)l=r9z-*l3bkx_4k!!z8mcyt246w1mfy!+8+#lxgra@wau4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["testserver", "localhost"]
 
